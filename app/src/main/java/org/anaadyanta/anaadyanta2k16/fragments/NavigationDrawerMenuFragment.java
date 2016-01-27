ebd655caf0cdbb1909b5cb37a2db35d44f61e7dc
Niranjan B <nero.niranjan@gmail.com>
2016-01-28 00:21:19 +0530

    added logic for switching between fragments on clicking in the navigation drawer menu

--- app/src/main/java/org/anaadyanta/anaadyanta2k16/fragments/NavigationDrawerMenuFragment.java
+++ app/src/main/java/org/anaadyanta/anaadyanta2k16/fragments/NavigationDrawerMenuFragment.java
@@ -1,7 +1,10 @@
 package org.anaadyanta.anaadyanta2k16.fragments;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -46,30 +49,52 @@ public class NavigationDrawerMenuFragment extends MenuFragment{
 
                 switch (item.getItemId()){
                     case R.id.news_feed:
+                        NewsFeedFragment newsFeedFragment = new NewsFeedFragment();
+                        FragmentManager fragmentManager = getFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.fragment_container, newsFeedFragment).commit();
                         Toast.makeText(getActivity(), "Clicked on News Feed!", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.home_fragment:
+                        HomeFragment homeFragment = new HomeFragment();
+                        FragmentManager homeFragmentManager = getFragmentManager();
+                        homeFragmentManager.beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
                         Toast.makeText(getActivity(), "Clicked on home fragment!", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.registration:
+                        RegistrationFragment registrationFragment = new RegistrationFragment();
+                        FragmentManager regFragmentManager = getFragmentManager();
+                        regFragmentManager.beginTransaction().replace(R.id.fragment_container, registrationFragment).commit();
                         Toast.makeText(getActivity(), "Clicked on registration fragment!", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.map:
+                        MapFragment mapFragment = new MapFragment();
+                        FragmentManager mapFragmentManager = getFragmentManager();
+                        mapFragmentManager.beginTransaction().replace(R.id.fragment_container, mapFragment).commit();
                         Toast.makeText(getActivity(), "Clicked on map fragment!", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.rules:
+                        RuleFragment ruleFragment = new RuleFragment();
+                        FragmentManager ruleFragmentManager = getFragmentManager();
+                        ruleFragmentManager.beginTransaction().replace(R.id.fragment_container, ruleFragment).commit();
                         Toast.makeText(getActivity(), "Clicked on rules fragment!", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.sponsors:
+                        SponsorsFragment sponsorsFragment = new SponsorsFragment();
+                        FragmentManager sponsorsFragmentManager = getFragmentManager();
+                        sponsorsFragmentManager.beginTransaction().replace(R.id.fragment_container, sponsorsFragment).commit();
                         Toast.makeText(getActivity(), "Clicked on sponsors fragment!", Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.devs:
+                        DevsFragment devsFragment = new DevsFragment();
+                        FragmentManager devsFragmentManager = getFragmentManager();
+                        devsFragmentManager.beginTransaction().replace(R.id.fragment_container, devsFragment).commit();
                         Toast.makeText(getActivity(), "Clicked on devs fragment!", Toast.LENGTH_SHORT).show();
                         return true;
                     default:
-                        Toast.makeText(getActivity(), "random clicks!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), "OOPS! That page ain't designed", Toast.LENGTH_SHORT).show();
                         return true;
                 }
+
             }
         });
         return setupReveal(view);
