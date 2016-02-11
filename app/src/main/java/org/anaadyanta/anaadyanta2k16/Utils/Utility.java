d505ba0d38ac33b8e703f1f1ec182058d85d929d
Niranjan B <nero.niranjan@gmail.com>
2016-02-11 10:53:42 +0530

    utlity class for checking network commection

--- app/src/main/java/org/anaadyanta/anaadyanta2k16/Utils/Utility.java
+++ app/src/main/java/org/anaadyanta/anaadyanta2k16/Utils/Utility.java
@@ -1,7 +1,21 @@
 package org.anaadyanta.anaadyanta2k16.Utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by NIRANJAN on 10-02-2016.
  */
 public class Utility {
+
+    private static Context mContext;
+
+    public static boolean isInternetAvailable(Context context) {
+        mContext = context;
+        ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
 }
