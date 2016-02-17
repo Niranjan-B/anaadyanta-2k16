fdf3782c1129230b2ae2c7f310ab2dfaa6448207
Niranjan B <nero.niranjan@gmail.com>
2016-02-18 00:16:08 +0530

    changed toolbar title as frgaments are clicked repsectively

--- app/src/main/java/org/anaadyanta/anaadyanta2k16/fragments/RuleFragment.java
+++ app/src/main/java/org/anaadyanta/anaadyanta2k16/fragments/RuleFragment.java
@@ -3,6 +3,8 @@ package org.anaadyanta.anaadyanta2k16.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +25,11 @@ public class RuleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        try {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Rulez");
+        } catch (NullPointerException exception) {
+            Log.d("anaadyanta", "" + exception.getMessage());
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rule, container, false);
     }
