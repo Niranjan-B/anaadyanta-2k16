fd6f576338e958ae899318f6241562b862ad630c
Niranjan B <nero.niranjan@gmail.com>
2016-02-23 00:33:29 +0530

    dialog pop-up on back pressed.

--- app/src/main/java/org/anaadyanta/anaadyanta2k16/Utils/Utility.java
+++ app/src/main/java/org/anaadyanta/anaadyanta2k16/Utils/Utility.java
@@ -1,8 +1,11 @@
 package org.anaadyanta.anaadyanta2k16.Utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 
 /**
  * Created by NIRANJAN on 10-02-2016.
@@ -16,6 +19,26 @@ public class Utility {
         ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public static void buildAlerDialog (final Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setPositiveButton("Yeah !", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ((Activity) context).finish();
+            }
+        });
+        builder.setNegativeButton("Not Really", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setMessage("Do you want to exit?");
+
+        AlertDialog exitDialog = builder.create();
+        exitDialog.show();
     }
 
 }
