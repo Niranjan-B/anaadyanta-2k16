4cd891e7261923c07c0fb7400b41960ef233a2d8
Niranjan B <nero.niranjan@gmail.com>
2016-02-24 22:45:24 +0530

    added splash screen.

---
+++ app/src/main/java/org/anaadyanta/anaadyanta2k16/SplashScreen.java
@@ -0,0 +1,38 @@
+package org.anaadyanta.anaadyanta2k16;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    sleep(3500);
+
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        background.start();
+    }
+}
